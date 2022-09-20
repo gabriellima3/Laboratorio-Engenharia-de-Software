72e84a2a63cab588edacb3f77611541dcfb9a944
Gabriel Lima <99371050+gabriellima3@users.noreply.github.com>
2022-09-20 17:18:33 -0300

    complete-version

--- vestibular-react/src/App.js
+++ vestibular-react/src/App.js
@@ -4,13 +4,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div class="App-sobre">
-        <img src="fatec.jpg" className="App-logo" alt="logo" />
+        <table>
+          <tr>
+            <td><img src="Fatec_Logo.png" className="App-logo" alt="logo" /></td>
+            <td><img src="logos.png" className="App-logo" alt="logo" /></td>
+          </tr>
+        </table>
+        <div class="App-sobre">
           <hr></hr>
-          <h4>MISSÃO</h4>
+          <h4 class="titulo">MISSÃO</h4>
           <p>Promover a educação tecnológica dentro de referenciais de excelência, formando profissionais com princípios éticos e responsabilidade social, capazes de contribuir ativamente e de forma inovadora para atender as demandas da sociedade.</p>
           <hr></hr>
-          <h4>VISÃO</h4>
+          <h4 class="titulo">VISÃO</h4>
           <p>Ser referência como instituição de ensino superior e pesquisa tecnológica, caracterizada pela inovação, ética e compromisso com o desenvolvimento sustentável.</p>
           <hr></hr>
           <img src="banner.png" className="App-logo" alt="logo" />
