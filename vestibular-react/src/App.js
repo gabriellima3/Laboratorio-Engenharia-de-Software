c6a141352f2e9145c746cc8bb2d6144a74321830
gabriellima3 <gabriel.ferreira68@fatec.sp.gov.br>
2022-09-19 22:12:36 -0300

    .

---
+++ vestibular-react/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div class="App-sobre">
+        <img src="fatec.jpg" className="App-logo" alt="logo" />
+          <hr></hr>
+          <h4>MISSÃO</h4>
+          <p>Promover a educação tecnológica dentro de referenciais de excelência, formando profissionais com princípios éticos e responsabilidade social, capazes de contribuir ativamente e de forma inovadora para atender as demandas da sociedade.</p>
+          <hr></hr>
+          <h4>VISÃO</h4>
+          <p>Ser referência como instituição de ensino superior e pesquisa tecnológica, caracterizada pela inovação, ética e compromisso com o desenvolvimento sustentável.</p>
+          <hr></hr>
+          <img src="banner.png" className="App-logo" alt="logo" />
+      </div>
+      
+
+      </header>
+ 
+    </div>
+  );
+}
+
+export default App;
