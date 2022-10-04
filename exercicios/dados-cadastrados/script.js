edfc41696d555e787e4411f31822bb2fe8ea09c5
Gabriel Lima <99371050+gabriellima3@users.noreply.github.com>
2022-10-04 00:40:37 -0300

    .

--- exercicios/dados-cadastrados/script.js
+++ exercicios/dados-cadastrados/script.js
@@ -1,9 +1,8 @@
 let cadastrarCampos = document.getElementById("cadastrar");
 cadastrarCampos.addEventListener("click", function () {
-    let elementos = [];
+    const elementos = [];
     let campos = document.getElementById("nome").value;
     elementos.push(campos.toString());
-
     let elementoOL = document.createElement("OL");
     elementos.forEach(element => {
         let elementLI = document.createElement("LI");
@@ -11,6 +10,21 @@ cadastrarCampos.addEventListener("click", function () {
         elementLI.appendChild(textNode);
         elementoOL.appendChild(textNode);
     });
-
     document.body.appendChild(elementoOL);
+
+    
+    let ordenarCampos = document.getElementById("ordenar");
+    ordenarCampos.addEventListener("click", function () {
+        let ordenados = elementos.sort();
+        let elementoOL = document.createElement("OL");
+        ordenados.forEach(element => {
+            let elementLI = document.createElement("LI");
+            let textNode = document.createTextNode(element);
+            elementLI.appendChild(textNode);
+            elementoOL.appendChild(textNode);
+        });
+        document.body.appendChild(elementoOL);
+    });
+
 });
+
--- vestibular-react/src/App.js
+++ vestibular-react/src/App.js
@@ -10,6 +10,7 @@ function App() {
             <td><img src="logos.png" className="App-logo" alt="logo" /></td>
           </tr>
         </table>
+
         <div class="App-sobre">
           <hr></hr>
           <h4 class="titulo">MISSÃO</h4>
@@ -18,12 +19,14 @@ function App() {
           <h4 class="titulo">VISÃO</h4>
           <p>Ser referência como instituição de ensino superior e pesquisa tecnológica, caracterizada pela inovação, ética e compromisso com o desenvolvimento sustentável.</p>
           <hr></hr>
-          <img src="banner.png" className="App-logo" alt="logo" />
-      </div>
-      
+          <a href="https://www.vestibularfatec.com.br/home/">
+            <img src="banner.png" className="App-logo" alt="logo" />
+          </a>
+        </div>
+
 
       </header>
- 
+
     </div>
   );
 }
